6cf960f90e04f768fc70092c027944c00509da9d
trevyn <230691+trevyn@users.noreply.github.com>
2023-12-31 18:35:25 +0400

    update Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,9 @@ fn main() -> eframe::Result<()> {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "improved-barnacle",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(improved_barnacle::TemplateApp::new(cc))),
     )
 }
 
